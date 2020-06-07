a624576e248d4b3554b531a276fb60fec2ffbb1c
kaiumi <amanda.lrib@gmail.com>
2020-06-07 17:21:41 -0300

    sera que foii?

--- helloWorld.c
+++ helloWorld.c
@@ -1,24 +1,17 @@
 #include <stdio.h>
 
 int main()
-{
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
+{   print(" S T O N KS\n");
     printf("Hello, alterando tudo denovo, pela segunda vez\n");
-=======
-=======
+
     printf("Hello, MasterMae\n");
->>>>>>> f62709841e22e12319dc71edc4a98d4992345844
     printf("Hello, Master nova\n");
     
->>>>>>> 5b5a5ab3ddddc9da8bf02535bbdc0879d4d3e781
     printf("Hello, A m a n d a\n");
-=======
+
     printf("Hello, Master\n");
     printf("Hello, MasterMae\n");
     printf("Hello, Dio!\n");
     
->>>>>>> 1c0612a65eaa60dd5c2d616b26651fb40290f523
     return 0;
 }
